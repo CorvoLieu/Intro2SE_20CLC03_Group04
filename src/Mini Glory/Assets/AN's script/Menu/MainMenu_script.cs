708134891d8e81a5fe1fc86cbecde7973bc7826b
NguyenDucTheAn <antheducnguyen@gmail.com>
2022-12-07 21:58:39 +0700

    2 last messages

--- src/Mini Glory/Assets/AN's script/Menu/MainMenu_script.cs
+++ src/Mini Glory/Assets/AN's script/Menu/MainMenu_script.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using Unity.Networking.Transport;
-
-
-
+using System;
 
 public class MainMenu_script : MonoBehaviour
 {
@@ -20,6 +18,9 @@ public class MainMenu_script : MonoBehaviour
     private int playerCount = -1;
     private int currentTeam = -1;
     private bool localGame = true;
+    private bool[] playerRematch = new bool[2];
+
+
 
 
     //Start is called before the first frame update
@@ -111,20 +112,37 @@ public class MainMenu_script : MonoBehaviour
     private void RegisterEvents()
     {
         NetUtility.S_WELCOME += OnWelcomeServer;
+        NetUtility.S_MAKE_MOVE += OnMakeMoveServer;
+        NetUtility.S_REMATCH += OnRematchServer;
 
         NetUtility.C_WELCOME += OnWelcomeClient;
         NetUtility.C_START_GAME += OnStartGameClient;
+        NetUtility.C_MAKE_MOVE += OnMakeMoveClient;
+        NetUtility.C_REMATCH += OnRematchClient;
     }
     private void UnRegisterEvents()
     {
         NetUtility.S_WELCOME -= OnWelcomeServer;
+        NetUtility.S_MAKE_MOVE -= OnMakeMoveServer;
+        NetUtility.S_REMATCH -= OnRematchServer;
 
         NetUtility.C_WELCOME -= OnWelcomeClient;
         NetUtility.C_START_GAME -= OnStartGameClient;
+        NetUtility.C_MAKE_MOVE -= OnMakeMoveClient;
+        NetUtility.C_REMATCH -= OnRematchClient;
     }
 
 
+
+
+
     // Server
+    private void OnMakeMoveServer(NetMessage msg, NetworkConnection cnn)
+    {
+        NetMakeMove mm = msg as NetMakeMove;
+        // Receive, and just broadcast it back
+        Server.Instance.Broadcast(msg);
+    }
     private void OnWelcomeServer(NetMessage msg, NetworkConnection cnn)
     {
         NetWelcome nw = msg as NetWelcome;
@@ -135,6 +153,11 @@ public class MainMenu_script : MonoBehaviour
         if (playerCount == 1)
             Server.Instance.Broadcast(new NetStartGame());
     }
+    private void OnRematchServer(NetMessage msg, NetworkConnection cnn)
+    {
+        Server.Instance.Broadcast(msg);
+    }
+
 
 
     // Client
@@ -149,6 +172,27 @@ public class MainMenu_script : MonoBehaviour
     {
         // We just need to go to the game scene
         SceneManager.LoadScene(1);
+    }
+    private void OnMakeMoveClient(NetMessage msg)
+    {
+        NetMakeMove mm = msg as NetMakeMove;
+
+        Debug.Log($"MM: {mm.teamId} : {mm.originalX} {mm.originalY} -> {mm.destinationX} {mm.destinationY}");
+
+        if (mm.teamId != currentTeam)
+        {
+            // MoveTo function
+        }
+    }
+    private void OnRematchClient(NetMessage msg)
+    {
+        NetRematch rm = msg as NetRematch;
+
+        playerRematch[rm.teamId] = rm.wantRematch == 1;
+
+        // activate the piece of UI
+
+        // if both players want to rematch
     }
     #endregion
 
